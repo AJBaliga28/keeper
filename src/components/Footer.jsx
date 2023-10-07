d9d22f3ea4c71a24b3a1d5f0e0f2e993374c5d65
Anirudh J Baliga <anirudh.baliga28@gmail.com>
2023-10-07 16:21:14 +0530

    Update Footer.jsx
    Added the GitHub profile link.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
+      <p> GitHub Profile: <a href='https://github.com/AJBaliga28/'> </p>
       <p>Copyright ⓒ {year}</p>
     </footer>
   );
